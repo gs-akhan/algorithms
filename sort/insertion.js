9d550536f3fbe06bb7b069f0e8b3e70f68f2a3d4
Azhar Uddin <akhan@gainsight.com>
2020-12-17 03:47:16 +0530

    Added Insertion Sort Documentation

--- sort/insertion.js
+++ sort/insertion.js
@@ -1,3 +1,11 @@
+/**
+ * We divide the array into the two parts, sorted and unsorted array. 
+ * The first part is sorted and second part is unsorted. 
+ * 
+ * We take one item from the unsorted and place it in the sorted array
+ * at the position it deserves.
+ *  
+ */
 function insertionSort(array) {
     for (let i = 1; i < array.length; i++) {
         let a = i;
